f56efebf18b6d86945a099fd1f6d8122baf5b01d
Bui Dai Nghia <6451071050@st.utc2.edu.vn>
2026-02-04 22:29:08 +0700

    set route for about

---
+++ Controllers/Client/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Public_Transport.Controllers.Client
+{
+    public class AboutController : Controller
+    {
+        [Route("/about")]
+        public IActionResult Index()
+        {
+            return View("~/Views/AboutUs/Index.cshtml");
+        }
+    }
+}
